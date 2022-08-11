d7449353dfc99775089763052c9b6e71a02b929f
JackSon <k9632441@gmail.com>
2022-08-11 16:13:58 +0900

    add stop actions

--- utils.py
+++ utils.py
@@ -22,7 +22,27 @@ def setGoHomeAction(_hermes_url: str = "http://192.168.11.1:1448/") -> bool:
     api_address = "api/multi-floor/motion/v1/gohomeaction"
     try:
         response = requests.post(_hermes_url + api_address, timeout=3)
-        return True
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+        
+    except Exception as e:
+        print("Timeout!! Return False")
+        return False
+    
+def deleteAction(_hermes_url: str = "http://192.168.11.1:1448/") -> bool:
+    if not _hermes_url[-1] == '/':
+        _hermes_url += '/'
+        
+    api_address = "api/core/motion/v1/actions/:current"
+    try:
+        response = requests.post(_hermes_url + api_address, timeout=3)
+        print(response)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
         
     except Exception as e:
         print("Timeout!! Return False")
@@ -30,15 +50,22 @@ def setGoHomeAction(_hermes_url: str = "http://192.168.11.1:1448/") -> bool:
     
 if __name__ == '__main__':
     url = "http://192.168.11.1:1448"
-        
+    go_home_flag = False
     go_home_threshold = 20
+    setGoHomeAction(url)
+
+    
     while True:
         battery_percent = getBatteryPercentage(url)
         print("Battery Percentage of Hermes is {}%".format(battery_percent))
         
-        if battery_percent < go_home_threshold:
+        if battery_percent < go_home_threshold and go_home_flag == False:
             print("Battery is lower than {}%, Set GoHome Action".format(go_home_threshold))
-            setGoHomeAction(url)    
-        else:
+            
+            go_home_flag = setGoHomeAction(url)
+        elif battery_percent >= go_home_threshold:
             print("Battery is enough")
+            go_home_flag = False
+        elif go_home_flag == True:
+            print("Battery is lower than {}%, Set GoHome Action".format(go_home_threshold))
         time.sleep(10)
